d7474b4daa75867780a77afde943e751514e9efd
Jiu <jiu@hawaii.edu>
2015-03-20 10:48:16 +0900

    added ICSFileParser

--- src/part2/Test.java
+++ src/part2/Test.java
@@ -4,6 +4,8 @@ public class Test {
 	public static void main(String[] args) {
 		ICSFileParser parse = new ICSFileParser("studyfinal.ics");
 		
+		System.out.println(parse.getDtstart());
 		System.out.println(parse.getDtend());
+		System.out.println(parse.getTzid());
 	}
 }
